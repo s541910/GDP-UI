56c48d0d5296b0f80e5acc5dac346d2f23c50cfc
anusha kanagala <67909209+Anushakanagala@users.noreply.github.com>
2021-10-24 23:12:33 -0500

    Added CommitteMember Dashboard

---
+++ src/components/CommitteMemberDashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class CommitteMemberDashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to CommitteMemberDashboard</h1>
+            </div>
+        )
+    }
+}
+
+export default CommitteMemberDashboard
