b13910ac74794eb36c747ab8febd9cac5e350fce
VENKATESH VEMULA <77640153+Vemula23@users.noreply.github.com>
2021-11-11 20:29:19 -0600

    Added Add company member to the components file

---
+++ src/components/AddCompanyMember.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class AddCompanyMember extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+            <h1> We are working on this page</h1>
+            </div>
+        )
+    }
+}
