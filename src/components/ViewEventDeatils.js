9b2f1b9aa454e4957966394f8823626007ffa404
anusha kanagala <67909209+Anushakanagala@users.noreply.github.com>
2021-12-05 18:38:46 -0600

    updated vieweventdetails

--- src/components/ViewEventDeatils.js
+++ src/components/ViewEventDeatils.js
@@ -12,15 +12,17 @@ export default class ViewEventDeatils extends Component {
     }
     componentDidMount() {
      EventService.getEventById(this.state.id).then((res) => {
-       console.log("sdkfbh",this.state.id);
-       console.log("ashdb",res.data);
        this.setState({ event: res.data })
      })
+  }
+  back()
+  {
+     this.props.history.goBack();
   }
     
     render() {
         return (
-    <div className="card col-md-6 offset-md-3">
+       <div className="card col-md-6 offset-md-3">
         <br></br>
           <h3 className="text-center"> View Event Details</h3>
           <div className="card-body">
@@ -28,18 +30,23 @@ export default class ViewEventDeatils extends Component {
               <label>Event  Name:
               <b>{this.state.event.eventname}</b></label>
             </div>
+            <div className= "row">
+              <label>Event  Description:
+              <b>{this.state.event.eventdescription}</b></label>
+            </div>
+            <div className= "row">
+              <label>Event  Type:
+              <b>{this.state.event.eventtype}</b></label>
+            </div>
             <div className="row">
               <label>Date: 
               <b>{this.state.event.eventdate}</b></label>
             </div>
             <div className="row">
-              <label> Email Id: 
-              <b>{this.state.event.emailid}</b></label>
-            </div>
-            <div className="row">
               <label> Location: 
               <b>{this.state.event.location}</b></label>
             </div>
+            <button style={{marginLeft:"10px"}} onClick={() => this.back()} className="btn btn-danger">Back</button> 
           </div>
         </div>
    
